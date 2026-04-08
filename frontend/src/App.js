1494c0cc0457efac15527889f5af34df99a2c4bb
Kabazin10 <257986885+Kabazin10@users.noreply.github.com>
2026-04-08 16:39:28 +0100

    Create App.js for the main React component of the musical festival application.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to the Musical Festival</h1>
+            <p>Your go-to app for all things music festivals!</p>
+        </div>
+    );
+};
+
+export default App;
